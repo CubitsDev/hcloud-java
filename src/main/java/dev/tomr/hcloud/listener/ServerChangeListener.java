af9d71bfbcd80ca0dfb8307a07361c92ab162573
Tom Roman <tom@tomroman.co.uk>
2025-01-01 17:29:59 +0000

    feat: Add Shutdown action
    - New method on Server to call shutdown
    - Uses same action service as the delete action for tracking

--- src/main/java/dev/tomr/hcloud/listener/ServerChangeListener.java
+++ src/main/java/dev/tomr/hcloud/listener/ServerChangeListener.java
@@ -22,6 +22,9 @@ public class ServerChangeListener implements PropertyChangeListener {
         if (evt.getPropertyName().equals("delete")) {
             logger.warn("Server delete has been called. Instructing Hetzner to delete");
             HetznerCloud.getInstance().getServiceManager().getServerService().deleteServerFromHetzner(server);
+        } else if (evt.getPropertyName().equals("shutdown")) {
+            logger.info("Server shutdown has been called. Instructing Hetzner to shut the server down");
+            HetznerCloud.getInstance().getServiceManager().getServerService().shutdownServer(server);
         } else {
             logger.info("Server changed: " + evt.getPropertyName());
             logger.info("Server: " + evt.getOldValue() + " -> " + evt.getNewValue());
